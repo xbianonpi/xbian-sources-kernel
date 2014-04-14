9a4528225b3773e8ed52d0f8ff35706f772dc926
Shengjiu Wang <b02247@freescale.com>
2014-04-14 14:58:05 +0800

    ENGR00307835-2 ASoC: dmaengine: Add two function for dmaengine_pcm
    Add check_xrun and device_reset for dmaengine_pcm
    
    Signed-off-by: Shengjiu Wang <b02247@freescale.com>
    (cherry picked from commit 405588f31e432e5c6634a0817e4d804ead87882b)
    (cherry picked from commit 43541a8742429f78368e8a430a43c8555dafe3b5)
    Signed-off-by: Matus Kral <matuskral@me.com>

--- include/sound/dmaengine_pcm.h
+++ include/sound/dmaengine_pcm.h
@@ -73,6 +73,8 @@ struct dma_chan *snd_dmaengine_pcm_get_chan(struct snd_pcm_substream *substream)
  * requesting the DMA channel.
  * @fifo_size: FIFO size of the DAI controller in bytes
  * @flags: PCM_DAI flags, only SND_DMAENGINE_PCM_DAI_FLAG_PACK for now
+ * @check_xrun: check if hardware xrun happen in the cpu dai.
+ * @device_reset: if xrun happened, then do cpu dai reset.
  */
 struct snd_dmaengine_dai_dma_data {
 	dma_addr_t addr;
@@ -82,6 +84,16 @@ struct snd_dmaengine_dai_dma_data {
 	void *filter_data;
 	unsigned int fifo_size;
 	unsigned int flags;
+	bool (*check_xrun)(struct snd_pcm_substream *substream);
+	void (*device_reset)(struct snd_pcm_substream *substream, bool stop);
+};
+
+struct dmaengine_pcm_runtime_data {
+	struct dma_chan *dma_chan;
+	dma_cookie_t cookie;
+
+	unsigned int pos;
+	dma_async_tx_callback callback;
 };
 
 void snd_dmaengine_pcm_set_config_from_dai_data(
