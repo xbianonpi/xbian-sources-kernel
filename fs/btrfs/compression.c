869fcf7fedfaea0eb3dd0632cdd4ced57fd704c3
Matus Kral <matuskral@me.com>
2015-05-16 04:39:02 +0200

    Btrfs: LZ4 compression patch

--- fs/btrfs/compression.c
+++ fs/btrfs/compression.c
@@ -763,6 +763,8 @@ static struct {
 static const struct btrfs_compress_op * const btrfs_compress_op[] = {
 	&btrfs_zlib_compress,
 	&btrfs_lzo_compress,
+	&btrfs_lz4_compress,
+	&btrfs_lz4hc_compress,
 };
 
 void __init btrfs_init_compress(void)
