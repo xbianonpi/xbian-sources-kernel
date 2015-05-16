ca20df5f37b41ad6035c2cfa34ca91ff83130252
Matus Kral <matuskral@me.com>
2015-05-16 04:25:40 +0200

    VPU352 patch (VPU OC control over kernel command line param)

--- arch/arm/mach-imx/mach-imx6q.c
+++ arch/arm/mach-imx/mach-imx6q.c
@@ -340,6 +340,12 @@ static void __init imx6q_opp_check_speed_grading(struct device *cpu_dev)
 			if (dev_pm_opp_disable(cpu_dev, 852000000))
 				pr_warn("failed to disable 852 MHz OPP\n");
 	}
+	if (vpu352) {
+		if (dev_pm_opp_disable(cpu_dev, 396000000))
+			pr_warn("VPU352: failed to disable 396MHz OPP\n");
+		pr_info("VPU352: remove 396MHz OPP for VPU running at 352MHz!\n");
+	}
+
 	iounmap(base);
 put_node:
 	of_node_put(np);
