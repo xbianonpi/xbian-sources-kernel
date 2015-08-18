1db109255cdfa023da72355d8ac1544998db1863
Jon Nettleton <jon.nettleton@gmail.com>
2015-08-18 07:01:19 +0200

    arm: imx: Clean up busfreq logging
    Switch to using pr_* functions and removing erroneous "\n" in the
    beginning of the messages to clean up the logging.

--- arch/arm/mach-imx/busfreq_lpddr2.c
+++ arch/arm/mach-imx/busfreq_lpddr2.c
@@ -72,7 +72,7 @@ int update_lpddr2_freq(int ddr_rate)
 	if (ddr_rate == curr_ddr_rate)
 		return 0;
 
-	printk(KERN_DEBUG "\nBus freq set to %d start...\n", ddr_rate);
+	pr_debug("Bus freq set to %d start...\n", ddr_rate);
 
 	/*
 	 * Flush the TLB, to ensure no TLB maintenance occurs
@@ -86,7 +86,7 @@ int update_lpddr2_freq(int ddr_rate)
 
 	curr_ddr_rate = ddr_rate;
 
-	printk(KERN_DEBUG "\nBus freq set to %d done...\n", ddr_rate);
+	pr_debug("Bus freq set to %d done...\n", ddr_rate);
 
 	return 0;
 }
