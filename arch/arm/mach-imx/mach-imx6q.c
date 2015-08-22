897036ddd3084abf96a44ca8de059a8ca25448e4
Jon Nettleton <jon.nettleton@gmail.com>
2015-08-22 13:04:56 +0200

    mach: imx: Re-enable SmartEEE in the Atheros Phy
    I am testing the atheros phy's built in power savings features,
    and did find that just enabling them was causing link disconnects
    as reported initially.  However since increasing the tx twd time
    out I have not experienced any more disconnects.
    
    This is a feature that I want to get working as it saves about 60mA's
    of power when the network is idle.
    
    (cherry picked from commit 9bfa9d0370c398dc4ce0b4d065933bf029274070)
    Signed-off-by: Matus Kral <matuskral@me.com>
    
    Conflicts:
    	arch/arm/mach-imx/mach-imx6q.c

--- arch/arm/mach-imx/mach-imx6q.c
+++ arch/arm/mach-imx/mach-imx6q.c
@@ -112,6 +112,22 @@ static int ar8031_phy_fixup(struct phy_device *dev)
 {
 	u16 val;
 
+	/* disable phy AR8031 SmartEEE function. */
+	phy_write(dev, 0xd, 0x3);
+	phy_write(dev, 0xe, 0x805d);
+	phy_write(dev, 0xd, 0x4003);
+	val = phy_read(dev, 0xe);
+	val |= (0x1 << 8);
+	phy_write(dev, 0xe, val);
+
+	/* Increase 1000BT tx time for SmartEEE. */
+	phy_write(dev, 0xd, 0x3);
+	phy_write(dev, 0xe, 0x805b);
+	phy_write(dev, 0xd, 0x4003);
+	val = phy_read(dev, 0xe);
+	val = 0x1317;
+	phy_write(dev, 0xe, val);
+
 	/* To enable AR8031 output a 125MHz clk from CLK_25M */
 	phy_write(dev, 0xd, 0x7);
 	phy_write(dev, 0xe, 0x8016);
