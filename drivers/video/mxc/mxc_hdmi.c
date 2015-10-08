26acdc38cc41937cbc097405a0a548dbd26792b0
Matus Kral <matuskral@me.com>
2015-10-08 08:01:55 +0200

    Revert "mxc_hdmi: present sysfs edid as binary data instead of ascii"
    This reverts commit 8abe8c715bd9822831adb6ae82c30b10fd1255b5.

--- drivers/video/mxc/mxc_hdmi.c
+++ drivers/video/mxc/mxc_hdmi.c
@@ -340,12 +340,16 @@ static ssize_t mxc_hdmi_show_edid(struct device *dev,
 		struct device_attribute *attr, char *buf)
 {
 	struct mxc_hdmi *hdmi = dev_get_drvdata(dev);
-	int j;
+	int i, j, len = 0;
 
-	for (j = 0; j < HDMI_EDID_LEN; j++)
-		buf[j] = hdmi->edid[j];
+	for (j = 0; j < HDMI_EDID_LEN/16; j++) {
+		for (i = 0; i < 16; i++)
+			len += sprintf(buf+len, "0x%02X ",
+					hdmi->edid[j*16 + i]);
+		len += sprintf(buf+len, "\n");
+	}
 
-	return HDMI_EDID_LEN;
+	return len;
 }
 
 static DEVICE_ATTR(edid, S_IRUGO, mxc_hdmi_show_edid, NULL);
