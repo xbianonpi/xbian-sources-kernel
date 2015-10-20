cf1504649922bf819e848eef525cfffac99d1091
Matus Kral <matuskral@me.com>
2015-10-20 02:51:49 +0200

    mxc_vout: add device_caps as 4.1.y v4l2 requires

--- drivers/media/platform/mxc/output/mxc_vout.c
+++ drivers/media/platform/mxc/output/mxc_vout.c
@@ -1013,7 +1013,8 @@ static int mxc_vidioc_querycap(struct file *file, void *fh,
 	strlcpy(cap->driver, VOUT_NAME, sizeof(cap->driver));
 	strlcpy(cap->card, vout->vfd->name, sizeof(cap->card));
 	cap->bus_info[0] = '\0';
-	cap->capabilities = V4L2_CAP_STREAMING | V4L2_CAP_VIDEO_OUTPUT;
+	cap->device_caps = V4L2_CAP_STREAMING | V4L2_CAP_VIDEO_OUTPUT;
+	cap->capabilities = cap->device_caps | V4L2_CAP_DEVICE_CAPS;
 
 	return 0;
 }
