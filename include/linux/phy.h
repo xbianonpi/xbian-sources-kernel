71150122b9a407e52dd970c1e9cf1398eaf585ca
Russell King <rmk+kernel@arm.linux.org.uk>
2015-12-28 13:51:16 +0100

    phy: export phy_speed_to_str() for phylink
    Upstream-commit: 45a27cfede3e2ea881024437a2673fc8a77b14a9
    
    phylink would like to reuse phy_speed_to_str() to convert the speed
    to a string.  Add a prototype and export this helper function.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -812,6 +812,7 @@ int phy_ethtool_ksettings_set(struct phy_device *phydev,
 			      const struct ethtool_link_ksettings *cmd);
 int phy_mii_ioctl(struct phy_device *phydev, struct ifreq *ifr, int cmd);
 int phy_start_interrupts(struct phy_device *phydev);
+const char *phy_speed_to_str(int speed);
 void phy_print_status(struct phy_device *phydev);
 void phy_device_free(struct phy_device *phydev);
 int phy_set_max_speed(struct phy_device *phydev, u32 max_speed);
