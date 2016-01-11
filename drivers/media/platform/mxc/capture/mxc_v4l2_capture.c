Gateworks/linux-imx6

mxc_capture: add tda1997x HDMI receiver video support
Signed-off-by: Tim Harvey <tharvey@gateworks.com>

drivers/media/platform/mxc/capture/mxc_v4l2_capture.c
@@ -1355,9 +1355,13 @@ static int mxc_v4l2_s_param(cam_data *cam, struct v4l2_streamparm *parm)
 	csi_param.mclk = 0;
 
 	pr_debug("   clock_curr=mclk=%d\n", ifparm.u.bt656.clock_curr);
-	if (ifparm.u.bt656.clock_curr == 0)
+	if (ifparm.u.bt656.clock_curr == 0) {
 		csi_param.clk_mode = IPU_CSI_CLK_MODE_CCIR656_INTERLACED;
-	else
+		/*protocol bt656 use 27Mhz pixel clock */
+		csi_param.mclk = 27000000;
+	} else if (ifparm.u.bt656.clock_curr == 1) {
+		csi_param.clk_mode = IPU_CSI_CLK_MODE_CCIR656_PROGRESSIVE;
+	} else
 		csi_param.clk_mode = IPU_CSI_CLK_MODE_GATED_CLK;
 
 	csi_param.pixclk_pol = ifparm.u.bt656.latch_clk_inv;

xbianonpi/xbian-sources-kernel

Upstream-commit: e9cba78905e59b4d3fe258671080892e92cbab14

drivers/media/platform/mxc/capture/mxc_v4l2_capture.c
@@ -1387,9 +1387,14 @@ void setup_ifparm(cam_data *cam, int init_defrect)
 //		csi_param.clk_mode = IPU_CSI_CLK_MODE_CCIR656_PROGRESSIVE;
 //		break;
 	default:
-		csi_param.clk_mode = (ifparm.u.bt656.clock_curr == 0) ?
-				IPU_CSI_CLK_MODE_CCIR656_INTERLACED :
-				IPU_CSI_CLK_MODE_GATED_CLK;
+		if (ifparm.u.bt656.clock_curr == 0) {
+			csi_param.clk_mode = IPU_CSI_CLK_MODE_CCIR656_INTERLACED;
+			/*protocol bt656 use 27Mhz pixel clock */
+			csi_param.mclk = 27000000;
+		} else if (ifparm.u.bt656.clock_curr == 1) {
+			csi_param.clk_mode = IPU_CSI_CLK_MODE_GATED_CLK;
+		} else
+			csi_param.clk_mode = IPU_CSI_CLK_MODE_GATED_CLK;
 	}
 
 	csi_param.pixclk_pol = ifparm.u.bt656.latch_clk_inv;
@@ -1572,7 +1577,6 @@ static int mxc_v4l2_s_param(cam_data *cam, struct v4l2_streamparm *parm)
 	/* If resolution changed, need to re-program the CSI */
 	/* Get new values. */
 	setup_ifparm(cam, 0);
-
 
 exit:
 	if (cam->overlay_on == true)