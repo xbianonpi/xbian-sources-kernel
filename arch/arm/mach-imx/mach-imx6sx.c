aa3d657e5f88909627fc80bd0b5eefa2320094b8
Matus Kral <matuskral@me.com>
2016-01-15 21:37:02 +0100

    mach-imx: add mxc_restart as .reboot function

--- arch/arm/mach-imx/mach-imx6sx.c
+++ arch/arm/mach-imx/mach-imx6sx.c
@@ -68,6 +68,8 @@ static void __init imx6sx_init_machine(void)
 {
 	struct device *parent;
 
+	mxc_arch_reset_init_dt();
+
 	parent = imx_soc_device_init();
 	if (parent == NULL)
 		pr_warn("failed to initialize soc device\n");
@@ -109,4 +111,5 @@ DT_MACHINE_START(IMX6SX, "Freescale i.MX6 SoloX (Device Tree)")
 	.init_machine	= imx6sx_init_machine,
 	.dt_compat	= imx6sx_dt_compat,
 	.init_late	= imx6sx_init_late,
+	.restart	= mxc_restart,
 MACHINE_END
