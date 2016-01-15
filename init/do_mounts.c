6367a4ae670e56d8f74a3443ca2d71d132738240
Matus Kral <matuskral@me.com>
2016-01-15 17:02:53 +0100

    init: mount as NOATIME by default

--- init/do_mounts.c
+++ init/do_mounts.c
@@ -37,7 +37,7 @@
 
 int __initdata rd_doload;	/* 1 = load RAM disk, 0 = don't load */
 
-int root_mountflags = MS_RDONLY | MS_SILENT;
+int root_mountflags = MS_RDONLY | MS_SILENT | MS_NOATIME;
 static char * __initdata root_device_name;
 static char __initdata saved_root_name[64];
 static int root_wait;
