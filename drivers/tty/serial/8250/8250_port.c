1d534137360af35af66f1d6ede60dbed03846bc7
Matus Kral <matuskral@me.com>
2016-05-09 00:15:24 +0200

    Revert "apply Real-Time patch -rt13"
    This reverts commit 4455ed29cc7a523a293bbe766a75aab04a7f9372.

--- drivers/tty/serial/8250/8250_port.c
+++ drivers/tty/serial/8250/8250_port.c
@@ -35,7 +35,6 @@
 #include <linux/nmi.h>
 #include <linux/mutex.h>
 #include <linux/slab.h>
-#include <linux/kdb.h>
 #include <linux/uaccess.h>
 #include <linux/pm_runtime.h>
 
@@ -2846,7 +2845,7 @@ void serial8250_console_write(struct uart_8250_port *up, const char *s,
 
 	if (port->sysrq)
 		locked = 0;
-	else if (oops_in_progress || in_kdb_printk())
+	else if (oops_in_progress)
 		locked = spin_trylock_irqsave(&port->lock, flags);
 	else
 		spin_lock_irqsave(&port->lock, flags);
