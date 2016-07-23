moonman/linux-fslc-sr

mmc: core: Don't fail UHS transition if host max_current fails.
During the transition to UHS we query the max_current of the host
and then attempt to negotiate that setting.  If it fails then
we exit the entire UHS transition.  Since this is the max_current
of the host and not the card try lower current settings until
they are accepted and we can move forward with the transition.

This should be safe since we are going from max_current down, and
any problems in the signalling should be caught when the voltage
switch happens.

drivers/mmc/core/sd.c
@@ -562,7 +562,7 @@ static u32 sd_get_host_max_current(struct mmc_host *host)
 static int sd_set_current_limit(struct mmc_card *card, u8 *status)
 {
 	int current_limit = SD_SET_CURRENT_NO_CHANGE;
-	int err;
+	int i, err;
 	u32 max_current;
 
 	/*
@@ -598,18 +598,23 @@ static int sd_set_current_limit(struct mmc_card *card, u8 *status)
 	else if (max_current >= 200)
 		current_limit = SD_SET_CURRENT_LIMIT_200;
 
-	if (current_limit != SD_SET_CURRENT_NO_CHANGE) {
-		err = mmc_sd_switch(card, 1, 3, current_limit, status);
+	if (current_limit == SD_SET_CURRENT_NO_CHANGE)
+		return 0;
+
+	for (i = current_limit; i >= 0; i--) { 
+		err = mmc_sd_switch(card, 1, 3, i, status);
 		if (err)
-			return err;
+			continue;
 
-		if (((status[15] >> 4) & 0x0F) != current_limit)
-			pr_warning("%s: Problem setting current limit!\n",
-				mmc_hostname(card->host));
-
+		if (((status[15] >> 4) & 0x0F) != i) {
+			pr_warning("%s: Problem setting current limit to %d!\n",
+				mmc_hostname(card->host), i);
+		} else {
+			break;
+		}
 	}
 
-	return 0;
+	return err;
 }
 
 /*

xbianonpi/xbian-sources-kernel

(cherry picked from commit 67f53c3d45db6636a26df48a71fef69139edd780)
Signed-off-by: Matus Kral <matuskral@me.com>
Conflicts:
	drivers/mmc/core/sd.c

drivers/mmc/core/sd.c
@@ -518,7 +518,7 @@ static u32 sd_get_host_max_current(struct mmc_host *host)
 static int sd_set_current_limit(struct mmc_card *card, u8 *status)
 {
 	int current_limit = SD_SET_CURRENT_NO_CHANGE;
-	int err;
+	int i, err;
 	u32 max_current;
 
 	/*
@@ -565,18 +565,23 @@ static int sd_set_current_limit(struct mmc_card *card, u8 *status)
 		 card->sw_caps.sd3_curr_limit & SD_MAX_CURRENT_200)
 		current_limit = SD_SET_CURRENT_LIMIT_200;
 
-	if (current_limit != SD_SET_CURRENT_NO_CHANGE) {
-		err = mmc_sd_switch(card, 1, 3, current_limit, status);
+	if (current_limit == SD_SET_CURRENT_NO_CHANGE)
+		return 0;
+
+	for (i = current_limit; i >= 0; i--) {
+		err = mmc_sd_switch(card, 1, 3, i, status);
 		if (err)
-			return err;
+			continue;
 
-		if (((status[15] >> 4) & 0x0F) != current_limit)
-			pr_warn("%s: Problem setting current limit!\n",
-				mmc_hostname(card->host));
-
+		if (((status[15] >> 4) & 0x0F) != i) {
+			pr_debug("%s: Problem setting current limit to %d!\n",
+				mmc_hostname(card->host), i);
+		} else {
+			break;
+		}
 	}
 
-	return 0;
+	return err;
 }
 
 /*