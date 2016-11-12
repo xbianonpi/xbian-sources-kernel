383f784c32f3abef6721e702c6ead478e3f09a17
Matus Kral <matuskral@me.com>
2016-11-12 16:01:24 +0100

    mxc-hdmi-core: ignore events from overlay fb

--- drivers/video/mxc/mxc_ipuv3_fb.c
+++ drivers/video/mxc/mxc_ipuv3_fb.c
@@ -59,47 +59,6 @@
 
 /* Display port number */
 #define MXCFB_PORT_NUM	2
-/*!
- * Structure containing the MXC specific framebuffer information.
- */
-struct mxcfb_info {
-	int default_bpp;
-	int cur_blank;
-	int next_blank;
-	ipu_channel_t ipu_ch;
-	int ipu_id;
-	int ipu_di;
-	u32 ipu_di_pix_fmt;
-	bool ipu_int_clk;
-	bool overlay;
-	bool alpha_chan_en;
-	bool late_init;
-	bool first_set_par;
-	dma_addr_t alpha_phy_addr0;
-	dma_addr_t alpha_phy_addr1;
-	void *alpha_virt_addr0;
-	void *alpha_virt_addr1;
-	uint32_t alpha_mem_len;
-	uint32_t ipu_ch_irq;
-	uint32_t ipu_ch_nf_irq;
-	uint32_t ipu_alp_ch_irq;
-	uint32_t cur_ipu_buf;
-	uint32_t cur_ipu_alpha_buf;
-
-	u32 pseudo_palette[16];
-
-	bool mode_found;
-	struct completion flip_complete;
-	struct completion alpha_flip_complete;
-	struct completion vsync_complete;
-
-	void *ipu;
-	struct fb_info *ovfbi;
-
-	struct mxc_dispdrv_handle *dispdrv;
-
-	struct fb_var_screeninfo cur_var;
-};
 
 struct mxcfb_pfmt {
 	u32 fb_pix_fmt;
