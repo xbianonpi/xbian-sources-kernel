a39636545815afb615f19dfca7c3aa959129592e
Matus Kral <matuskral@me.com>
2017-03-04 08:08:05 +0100

    Revert "MLK-9669-1 arm: imx: skip system bus related clks parent switch check"
    This reverts commit 6bd21ac1e14367168cda6d49a6fb756a33694bab.

--- drivers/clk/imx/clk.h
+++ drivers/clk/imx/clk.h
@@ -203,14 +203,6 @@ static inline struct clk *imx_clk_gate4(const char *name, const char *parent,
 			reg, shift, 0x3, 0, &imx_ccm_lock, NULL);
 }
 
-static inline struct clk *imx_clk_mux_bus(const char *name, void __iomem *reg,
-		u8 shift, u8 width, const char **parents, int num_parents)
-{
-	return clk_register_mux(NULL, name, parents, num_parents,
-			CLK_SET_RATE_NO_REPARENT,
-			reg, shift, width, 0, &imx_ccm_lock);
-}
-
 static inline struct clk *imx_clk_mux(const char *name, void __iomem *reg,
 		u8 shift, u8 width, const char **parents, int num_parents)
 {
